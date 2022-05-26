4a80e143fc79c2b9b8132abaf7d998a9687dfda2
SShipon <nxshipon@gmail.com>
2022-05-26 12:30:02 +0600

    code updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { response } = require("express");
 require("dotenv").config();
@@ -9,6 +10,7 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -27,20 +29,50 @@ async function run() {
   try {
     await client.connect();
     const productCollection = client.db("Bike").collection("products");
-     
+    const reviewCollection = client.db("user-Review").collection("reviews");
 
-    app.get('/product', async (req, res) => {
-      const query = {}
-      const cursor = productCollection.find(query)
-      const products = await cursor.toArray()
-      res.send(products)
-    })
-    app.get('/product/:id', async (req, res) => {
+    /* product database get */
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: ObjectId(id) }
-      const product = await productCollection.findOne(query)
-      res.send(product)
-    })
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
+    });
+
+    /* Product add */
+    
+     app.post("/bikeAdd", async (req, res) => {
+       const result = await productCollection.insertOne(req.body);
+       res.send(result);
+     });
+
+    
+    
+
+
+    /*=========== add your reviews add ===============  */
+
+    // add review
+    app.post("/reviews", async (req, res) => {
+      const result = await reviewCollection.insertOne(req.body);
+      res.send(result);
+    });
+
+    // get review
+     app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find({}).toArray();
+      res.send(result);
+     }); 
+    
+    
+    
+    
 
   } finally {
 
@@ -55,5 +87,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("server site running assignment-11", port);
+  console.log("server site running assignment-12", port);
 });
